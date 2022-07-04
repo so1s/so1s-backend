7d97f1df20a0611ca520e7bb901e82c5052b7a3d
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-07-04 19:43:20 +0900

    refactor: add annotation

--- src/main/java/io/so1s/backend/domain/model/dto/request/ModelUploadRequestDto.java
+++ src/main/java/io/so1s/backend/domain/model/dto/request/ModelUploadRequestDto.java
@@ -1,9 +1,15 @@
 package io.so1s.backend.domain.model.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ModelUploadRequestDto {
 
   @NotNull
