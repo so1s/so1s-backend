2ac1b8b5aa0ce0f3de3e33b0b183fb67097f9a81
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-07-05 15:10:37 +0900

    feat: add ObjectMapper as bean
    Co-authored-by: jiho lee <optional.int@kakao.com>
    Co-authored-by: shinilseop <shinilseop12@gmail.com>

---
+++ src/main/java/io/so1s/backend/global/config/JacksonConfig.java
@@ -0,0 +1,135 @@
+package io.so1s.backend.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.cfg.MapperConfig;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
+import io.so1s.backend.global.utils.ObjectMapperFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@RequiredArgsConstructor
+public class JacksonConfig {
+
+  @Bean
+  @Primary
+  @Qualifier("camelObjectMapper")
+  public ObjectMapper camelObjectMapper() {
+    return ObjectMapperFactory.create(camelNamingStrategy());
+  }
+
+  @Bean
+  @Qualifier("snakeObjectMapper")
+  public ObjectMapper snakeObjectMapper() {
+    return ObjectMapperFactory.create(snakeNamingStrategy());
+  }
+
+  @Bean
+  @Primary
+  public PropertyNamingStrategy camelNamingStrategy() {
+    return new CamelNamingStrategy();
+  }
+
+  @Bean
+  public PropertyNamingStrategy snakeNamingStrategy() {
+    return new SnakeNamingStrategy();
+  }
+
+  public static class CamelNamingStrategy extends
+      PropertyNamingStrategies.LowerCamelCaseStrategy {
+
+    @Override
+    public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method,
+        String defaultName) {
+      if (method.getParameterCount() == 1 &&
+          (method.getRawParameterType(0) == Boolean.class
+              || method.getRawParameterType(0) == boolean.class) &&
+          method.getName().startsWith("set")) {
+
+        Class<?> containingClass = method.getDeclaringClass();
+        String potentialFieldName = "is" + method.getName().substring(3);
+
+        try {
+          containingClass.getDeclaredField(potentialFieldName);
+          return potentialFieldName;
+        } catch (NoSuchFieldException e) {
+          // do nothing and fall through
+        }
+      }
+
+      return super.nameForSetterMethod(config, method, defaultName);
+    }
+
+    @Override
+    public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method,
+        String defaultName) {
+      if (method.hasReturnType() && (method.getRawReturnType() == Boolean.class
+          || method.getRawReturnType() == boolean.class)
+          && method.getName().startsWith("is")) {
+
+        Class<?> containingClass = method.getDeclaringClass();
+        String potentialFieldName = method.getName();
+
+        try {
+          containingClass.getDeclaredField(potentialFieldName);
+          return potentialFieldName;
+        } catch (NoSuchFieldException e) {
+          // do nothing and fall through
+        }
+      }
+      return super.nameForGetterMethod(config, method, defaultName);
+    }
+
+
+  }
+
+  public static class SnakeNamingStrategy extends PropertyNamingStrategies.SnakeCaseStrategy {
+
+    @Override
+    public String nameForSetterMethod(MapperConfig<?> config, AnnotatedMethod method,
+        String defaultName) {
+      if (method.getParameterCount() == 1 &&
+          (method.getRawParameterType(0) == Boolean.class
+              || method.getRawParameterType(0) == boolean.class) &&
+          method.getName().startsWith("set")) {
+
+        Class<?> containingClass = method.getDeclaringClass();
+        String potentialFieldName = "is" + method.getName().substring(3);
+
+        try {
+          containingClass.getDeclaredField(potentialFieldName);
+          return potentialFieldName;
+        } catch (NoSuchFieldException e) {
+          // do nothing and fall through
+        }
+      }
+
+      return super.nameForSetterMethod(config, method, defaultName);
+    }
+
+    @Override
+    public String nameForGetterMethod(MapperConfig<?> config, AnnotatedMethod method,
+        String defaultName) {
+      if (method.hasReturnType() && (method.getRawReturnType() == Boolean.class
+          || method.getRawReturnType() == boolean.class)
+          && method.getName().startsWith("is")) {
+
+        Class<?> containingClass = method.getDeclaringClass();
+        String potentialFieldName = method.getName();
+
+        try {
+          containingClass.getDeclaredField(potentialFieldName);
+          return potentialFieldName;
+        } catch (NoSuchFieldException e) {
+          // do nothing and fall through
+        }
+      }
+      return super.nameForGetterMethod(config, method, defaultName);
+    }
+  }
+}
