ff16081cf67a5813d32a78e7820124eef6ad5c25
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-07-05 15:09:52 +0900

    fix: model upload request dto as body
    Co-authored-by: jiho lee <optional.int@kakao.com>
    Co-authored-by: shinilseop <shinilseop12@gmail.com>

--- src/main/java/io/so1s/backend/domain/model/controller/ModelController.java
+++ src/main/java/io/so1s/backend/domain/model/controller/ModelController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ public class ModelController {
 
   @PostMapping
   public ResponseEntity<ModelUploadResponseDto> upload(
-      @Valid ModelUploadRequestDto modelUploadRequestDto) {
+      @RequestBody @Valid ModelUploadRequestDto modelUploadRequestDto) {
 
     ModelUploadResponseDto result = modelService.save(modelUploadRequestDto);
 
