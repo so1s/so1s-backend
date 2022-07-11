20059d346eb8e0697b6e652f91c1d3fcd5d4a891
jiho lee <optional.int@kakao.com>
2022-07-11 17:13:35 +0900

    feat: add UserDetailsServiceImpl

---
+++ src/main/java/io/so1s/backend/global/domain/auth/service/UserDetailsServiceImpl.java
@@ -0,0 +1,38 @@
+package io.so1s.backend.global.domain.auth.service;
+
+import io.so1s.backend.global.domain.auth.repository.UserRepository;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  @Transactional(readOnly = true)
+  public UserDetails loadUserByUsername(final String username) {
+    return userRepository.findByUsername(username)
+        .map(this::getUser)
+        .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+  }
+
+  private org.springframework.security.core.userdetails.User getUser(
+      io.so1s.backend.global.domain.auth.entity.User user) {
+    List<GrantedAuthority> grantedAuthorities = user.getUserRole().stream()
+        .map(authority -> new SimpleGrantedAuthority(authority.getUserRole().toString()))
+        .collect(Collectors.toList());
+    return new org.springframework.security.core.userdetails.User(user.getUsername(),
+        user.getPassword(),
+        grantedAuthorities);
+  }
+}
