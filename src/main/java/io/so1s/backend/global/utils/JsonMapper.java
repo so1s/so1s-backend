1d3b5721df6ca3bfb25e5fa081ababea3b8b15fc
jiho lee <optional.int@kakao.com>
2022-07-11 18:08:49 +0900

    feat: use JsonMapper

---
+++ src/main/java/io/so1s/backend/global/utils/JsonMapper.java
@@ -0,0 +1,32 @@
+package io.so1s.backend.global.utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.test.web.servlet.MvcResult;
+
+@Component
+public class JsonMapper {
+
+  @Autowired
+  @Qualifier("camelObjectMapper")
+  private ObjectMapper objectMapper;
+
+  public String asJsonString(final Object obj) {
+    try {
+      return objectMapper.writeValueAsString(obj);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public <T> T fromMvcResult(MvcResult result, Class<T> clazz) throws Exception {
+    return objectMapper.readValue(result.getResponse().getContentAsString(), clazz);
+  }
+
+  public <T> T fromMvcResult(MvcResult result, TypeReference<T> typeReference) throws Exception {
+    return objectMapper.readValue(result.getResponse().getContentAsString(), typeReference);
+  }
+}
