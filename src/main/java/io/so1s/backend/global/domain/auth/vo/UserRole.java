7c0ad3d9f5bea596b89ceb62b0242bb18717a288
jiho lee <optional.int@kakao.com>
2022-07-11 17:13:20 +0900

    feat: add UserRole enum

---
+++ src/main/java/io/so1s/backend/global/domain/auth/vo/UserRole.java
@@ -0,0 +1,15 @@
+package io.so1s.backend.global.domain.auth.vo;
+
+import lombok.ToString;
+
+// 상위 역할의 사용자는 하위 권한의 사용자 계정을 생성할 수 있고,
+// 하위 역할이 할 수 있는 일을 수행할 수 있다.
+@ToString
+public enum UserRole {
+  // 앱을 실행할 때 최초로 계정이 발급되는 가장 높은 권한
+  OWNER,
+  // API 서버의 관리자 역할
+  ADMIN,
+  // 인퍼런스 서버의 사용자 역할
+  USER
+}
