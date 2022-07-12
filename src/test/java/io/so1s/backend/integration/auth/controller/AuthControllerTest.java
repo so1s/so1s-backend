0a6b82beb557a585673602ac5f7831fbc8357cba
jiho lee <optional.int@kakao.com>
2022-07-12 16:38:31 +0900

    chore: Flush DB after each test

--- src/test/java/io/so1s/backend/integration/auth/controller/AuthControllerTest.java
+++ src/test/java/io/so1s/backend/integration/auth/controller/AuthControllerTest.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -31,6 +32,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureRestDocs
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+// Flush DB after each test
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+// For use test jwt secret
 @ActiveProfiles(profiles = {"test"})
 class AuthControllerTest {
 
