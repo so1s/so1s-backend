7cbb62c93c46d0b8ea7000a4cf37ae050e9dbc22
jiho lee <optional.int@kakao.com>
2022-07-12 19:01:10 +0900

    feat: Register Test

--- src/test/java/io/so1s/backend/integration/auth/controller/AuthControllerTest.java
+++ src/test/java/io/so1s/backend/integration/auth/controller/AuthControllerTest.java
@@ -7,9 +7,12 @@ import io.so1s.backend.domain.auth.dto.request.SignUpRequestDto;
 import io.so1s.backend.domain.auth.dto.response.SignUpResponseDto;
 import io.so1s.backend.domain.auth.dto.response.TokenResponseDto;
 import io.so1s.backend.domain.auth.entity.User;
+import io.so1s.backend.domain.auth.entity.UserToRole;
 import io.so1s.backend.domain.auth.service.UserService;
 import io.so1s.backend.domain.auth.vo.UserRole;
 import io.so1s.backend.global.utils.JsonMapper;
+import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,6 +28,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
@@ -56,13 +60,16 @@ class AuthControllerTest {
 
   private String token;
 
-  @Test
-  @DisplayName("기존에 생성된 Owner Role 계정을 통해 로그인할 수 있다.")
-  void testSignIn() throws Exception {
+  User createUser(String username, String password, UserRole userRole) throws Exception {
     // given
-    user = userService.createUser("owner", "so1s", UserRole.OWNER);
+    user = userService.createUser(username, password, userRole);
 
-    LoginRequestDto loginRequestDto = LoginRequestDto.builder().username("owner").password("so1s")
+    return user;
+  }
+
+  String getToken(String username, String password) throws Exception {
+    LoginRequestDto loginRequestDto = LoginRequestDto.builder().username(username)
+        .password(password)
         .build();
 
     String requestBody = jsonMapper.asJsonString(loginRequestDto);
@@ -94,37 +101,91 @@ class AuthControllerTest {
         //then
         .andExpect(MockMvcResultMatchers.status().isOk());
 
+    return token;
+  }
+
+  @Test
+  @DisplayName("기존에 생성된 Owner Role 계정을 통해 로그인할 수 있다.")
+  void testSignIn() throws Exception {
+
+    List<String> usernames = List.of("owner1", "admin1", "user1");
+    List<UserRole> userRoles = List.of(UserRole.OWNER, UserRole.ADMIN, UserRole.USER);
+    String password = "so1s";
+
+    for (int i = 0; i < usernames.size(); i++) {
+      String username = usernames.get(i);
+      UserRole userRole = userRoles.get(i);
+
+      createUser(username, password, userRole);
+      getToken(username, password);
+    }
   }
 
   @Test
   @DisplayName("기존에 생성된 Owner Role 계정이 Admin Role 계정을 생성할 수 있다.")
   void testSignUp() throws Exception {
     // given
-    testSignIn();
+    List<String> usernames = List.of("owner1", "admin1", "user1");
+    List<String> newUsernames = List.of("owner2", "admin2", "user2", "user3");
+    List<UserRole> userRoles = List.of(UserRole.OWNER, UserRole.ADMIN, UserRole.USER);
+    String password = "so1s";
 
-    SignUpRequestDto signUpRequestDto = SignUpRequestDto.builder()
-        .username("admin")
-        .password("so1s")
-        .build();
+    createUser("owner", password, UserRole.OWNER);
+    getToken("owner", password);
 
-    String requestBody = jsonMapper.asJsonString(signUpRequestDto);
+    for (int i = 1; i < usernames.size() + 1; i++) {
+      String username = usernames.get(i - 1);
+      UserRole userRole = userRoles.get(i - 1);
 
-    // when
-    MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders
-            .post(signUpEndPoint)
-            .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(requestBody))
-        .andExpect(MockMvcResultMatchers.status().is(HttpStatus.CREATED.value()))
-        .andReturn();
+      String newUsername = newUsernames.get(i);
 
-    // then
-    SignUpResponseDto signUpResponseDto = jsonMapper.fromMvcResult(mvcResult,
-        SignUpResponseDto.class);
+      createUser(username, password, userRole);
+      getToken(username, password);
 
-    assertThat(signUpResponseDto.getSuccess()).isTrue();
+      SignUpRequestDto signUpRequestDto = SignUpRequestDto.builder()
+          .username(newUsername)
+          .password(password)
+          .build();
 
-    assertThat(userService.findByUsername("admin")).isPresent();
+      String requestBody = jsonMapper.asJsonString(signUpRequestDto);
+
+      // when
+      ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders
+          .post(signUpEndPoint)
+          .header(HttpHeaders.AUTHORIZATION, String.format("Bearer %s", token))
+          .accept(MediaType.APPLICATION_JSON)
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(requestBody));
+
+      HttpStatus status = i == 3 ? HttpStatus.BAD_REQUEST : HttpStatus.CREATED;
+
+      // then
+      MvcResult mvcResult = resultActions.andExpect(
+              MockMvcResultMatchers.status().is(status.value()))
+          .andReturn();
+
+      if (i == 3) {
+        continue;
+      }
+
+      SignUpResponseDto signUpResponseDto = jsonMapper.fromMvcResult(mvcResult,
+          SignUpResponseDto.class);
+
+      assertThat(signUpResponseDto.getSuccess()).isTrue();
+
+      Optional<User> optionalCreatedUser = userService.findByUsername(newUsername);
+
+      assertThat(optionalCreatedUser).isPresent();
+
+      User createdUser = optionalCreatedUser.get();
+      UserRole newUserRole = userRoles.get(i);
+
+      assertThat(createdUser.getUsername()).isEqualTo(newUsername);
+      assertThat(createdUser.getUserToRoles().stream().map(UserToRole::getUserRole))
+          .contains(newUserRole);
+
+      user = createdUser;
+    }
+
   }
 }
