bcb9e78219d49a026f10a85f2419f559fbbbf174
jiho lee <optional.int@kakao.com>
2022-07-13 18:59:07 +0900

    chore: remove column names

--- src/main/java/io/so1s/backend/domain/auth/entity/User.java
+++ src/main/java/io/so1s/backend/domain/auth/entity/User.java
@@ -31,16 +31,15 @@ public class User {
 
   @JsonIgnore
   @Id
-  @Column(name = "user_id")
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long userId;
+  private Long id;
 
-  @Column(name = "username", length = 50, unique = true)
+  @Column(length = 50, unique = true)
   private String username;
 
   @JsonIgnore
   @ToString.Exclude
-  @Column(name = "password", length = 100)
+  @Column(length = 100)
   private String password;
 
   @Fetch(FetchMode.JOIN)
