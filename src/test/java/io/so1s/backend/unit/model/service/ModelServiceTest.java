b0fe429c73e0b568de0efd714e47e58491e8946d
shinilseop <shinilseop12@gmail.com>
2022-07-18 16:57:50 +0900

    test: fix controller (integration to unit)

--- src/test/java/io/so1s/backend/unit/model/service/ModelServiceTest.java
+++ src/test/java/io/so1s/backend/unit/model/service/ModelServiceTest.java
@@ -7,11 +7,13 @@ import static org.mockito.Mockito.when;
 import io.so1s.backend.domain.kubernetes.service.KubernetesService;
 import io.so1s.backend.domain.model.dto.request.ModelUploadRequestDto;
 import io.so1s.backend.domain.model.dto.response.ModelUploadResponseDto;
+import io.so1s.backend.domain.model.entity.Model;
 import io.so1s.backend.domain.model.entity.ModelMetadata;
 import io.so1s.backend.domain.model.repository.ModelMetadataRepository;
 import io.so1s.backend.domain.model.repository.ModelRepository;
 import io.so1s.backend.domain.model.service.ModelServiceImpl;
 import io.so1s.backend.global.utils.HashGenerator;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -32,38 +34,61 @@ class ModelServiceTest {
   @InjectMocks
   ModelServiceImpl modelService;
 
-  String name = "testModel";
-  String url = "http://s3.test.com/";
-  String library = "tensorflow";
-  String info = "this is test model.";
+  ModelUploadRequestDto requestDto;
+  String version;
+  Model model;
+  ModelMetadata modelMetadata;
 
-  @Test
-  @DisplayName("모델을 업로드 한다.")
-  public void modelUpload() throws Exception {
-    // given
-    ModelUploadRequestDto requestDto = ModelUploadRequestDto.builder()
+  @BeforeEach
+  public void setup() {
+    String name = "testModel";
+    String url = "http://s3.test.com/";
+    String library = "tensorflow";
+    String info = "this is test model.";
+
+    requestDto = ModelUploadRequestDto.builder()
         .name(name)
         .url(url)
         .library(library)
         .info(info)
         .build();
-    String version = HashGenerator.sha256();
-    ModelMetadata modelMetadata = ModelMetadata.builder()
+    model = requestDto.toEntity();
+    version = HashGenerator.sha1();
+    modelMetadata = ModelMetadata.builder()
         .url(url)
         .version(version)
         .info(info)
         .status("pending")
+        .model(model)
         .build();
-    when(modelRepository.save(any())).thenReturn(requestDto.toEntity());
+  }
+
+  @Test
+  @DisplayName("모델을 업로드 한다.")
+  public void modelUpload() throws Exception {
+    // given
+    when(modelRepository.save(any())).thenReturn(model);
     when(modelMetadataRepository.save(any())).thenReturn(modelMetadata);
-    when(kubernetesService.inferenceServerBuild(any(), any())).thenReturn(true);
+    when(kubernetesService.inferenceServerBuild(any())).thenReturn(true);
 
     // when
-    ModelUploadResponseDto result = modelService.upload(requestDto);
+    Model resultModel = modelService.createModel(requestDto);
+    ModelMetadata resultModelMetadata = modelService.createModelMetadata(resultModel, requestDto);
+    ModelUploadResponseDto result = modelService.buildModel(resultModelMetadata);
 
     //then
-    assertThat(name).isEqualTo(result.getName());
-    assertThat(version).isEqualTo(result.getVersion());
     assertThat(result.getSuccess()).isTrue();
+    assertThat(requestDto.getName()).isEqualTo(result.getName());
+    assertThat(version).isEqualTo(result.getVersion());
+  }
+
+  @Test
+  public void findModelMetadataByVersion() throws Exception {
+    // given
+
+    // when
+
+    // then
+
   }
 }
