e1563491e5bb84f82e3137101b2c82ed7e7882c1
shinilseop <shinilseop12@gmail.com>
2022-07-24 00:51:22 +0900

    test: add model service,controller test

--- src/test/java/io/so1s/backend/unit/model/service/ModelServiceTest.java
+++ src/test/java/io/so1s/backend/unit/model/service/ModelServiceTest.java
@@ -1,75 +1,121 @@
 package io.so1s.backend.unit.model.service;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.server.mock.EnableKubernetesMockClient;
-import io.so1s.backend.domain.kubernetes.service.KubernetesService;
-import io.so1s.backend.domain.model.dto.response.ModelUploadResponseDto;
+import io.so1s.backend.domain.aws.service.FileSaveResultForm;
+import io.so1s.backend.domain.model.dto.request.ModelUploadRequestDto;
 import io.so1s.backend.domain.model.entity.Model;
 import io.so1s.backend.domain.model.entity.ModelMetadata;
 import io.so1s.backend.domain.model.repository.ModelMetadataRepository;
 import io.so1s.backend.domain.model.repository.ModelRepository;
 import io.so1s.backend.domain.model.service.ModelServiceImpl;
+import io.so1s.backend.global.error.exception.DuplicateModelNameException;
 import io.so1s.backend.global.utils.HashGenerator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
-@EnableKubernetesMockClient(crud = true)
+@DataJpaTest
 @ExtendWith(MockitoExtension.class)
 @ActiveProfiles(profiles = {"test"})
 class ModelServiceTest {
 
-  KubernetesClient client;
-
-
+  @InjectMocks
   ModelServiceImpl modelService;
-  KubernetesService kubernetesService;
-  @Mock
+  @Autowired
   ModelRepository modelRepository;
-  @Mock
+  @Autowired
   ModelMetadataRepository modelMetadataRepository;
 
-  String version;
   String name = "testModel";
-  String url = "http://s3.test.com/";
   String library = "tensorflow";
-  String info = "this is test model.";
+  String version;
+  ModelUploadRequestDto modelUploadRequestDto;
 
   @BeforeEach
   public void setup() {
     version = HashGenerator.sha256();
-    kubernetesService = new KubernetesService(client);
-    modelService = new ModelServiceImpl(modelRepository, modelMetadataRepository,
-        kubernetesService);
+    modelService = new ModelServiceImpl(modelRepository, modelMetadataRepository);
+
+    modelUploadRequestDto = ModelUploadRequestDto.builder()
+        .name(name)
+        .library(library)
+        .inputShape("(10,)")
+        .inputDtype("float32")
+        .outputShape("(1,)")
+        .outputDtype("float32")
+        .build();
+  }
+
+
+  @Test
+  @Transactional
+  @DisplayName("모델을 저장한다.")
+  public void createModelTest() throws Exception {
+    // given
+    // setup()
+
+    // when
+    modelService.createModel(modelUploadRequestDto);
+    Model result = modelRepository.findByName(modelUploadRequestDto.getName()).get();
+
+    // then
+    assertThat(result.getName()).isEqualTo(modelUploadRequestDto.getName());
+    assertThat(result.getLibrary()).isEqualTo(modelUploadRequestDto.getLibrary());
   }
 
   @Test
-  @DisplayName("저장한 모델을 인퍼런스서버로 빌드한다.")
-  public void modelUpload() throws Exception {
+  @Transactional
+  @DisplayName("모델 이름이 중복되면 DuplicateModelNameException이 발생한다.")
+  public void createModelExceptionTest() throws Exception {
     // given
-    ModelMetadata modelMetadata = ModelMetadata.builder()
-        .url(url)
-        .version(version)
-        .info(info)
-        .status("pending")
-        .model(Model.builder()
-            .name(name)
-            .library(library)
-            .build())
+    // setup()
+    ModelUploadRequestDto modelUploadRequestDto2 = ModelUploadRequestDto.builder()
+        .name(name)
+        .library(library)
+        .inputShape("(10,)")
+        .inputDtype("float32")
+        .outputShape("(1,)")
+        .outputDtype("float32")
         .build();
 
     // when
-    ModelUploadResponseDto result = modelService.buildModel(modelMetadata);
+    modelService.createModel(modelUploadRequestDto);
 
-    //then
-    assertThat(result.getSuccess()).isTrue();
-    assertThat(modelMetadata.getModel().getName()).isEqualTo(result.getName());
-    assertThat(modelMetadata.getVersion()).isEqualTo(result.getVersion());
+    // then
+    assertThrows(DuplicateModelNameException.class, () -> {
+      modelService.createModel(modelUploadRequestDto2);
+    });
+  }
+
+  @Test
+  @Transactional
+  @DisplayName("모델 메타데이터를 저장한다.")
+  public void createModelMetadataTest() throws Exception {
+    // given
+    // setup()
+    FileSaveResultForm saveResult = FileSaveResultForm.builder()
+        .fileName("fileName")
+        .url("http://test.com/")
+        .build();
+
+    // when
+    ModelMetadata modelMetadata = modelService.createModelMetadata(
+        modelUploadRequestDto.toModelEntity(), modelUploadRequestDto,
+        saveResult);
+    ModelMetadata result = modelMetadataRepository.findById(modelMetadata.getId()).get();
+
+    // then
+    assertThat(result.getModel().getName()).isEqualTo(modelUploadRequestDto.getName());
+    assertThat(result.getUrl()).isEqualTo(saveResult.getUrl());
+    assertThat(result.getFileName()).isEqualTo(saveResult.getFileName());
   }
 }
