41564491fb030e38fc68f1ef247147325a79b6de
shinilseop <shinilseop12@gmail.com>
2022-08-06 02:10:44 +0900

    feat: add update deployment

--- src/main/java/io/so1s/backend/domain/deployment/controller/DeploymentController.java
+++ src/main/java/io/so1s/backend/domain/deployment/controller/DeploymentController.java
@@ -3,14 +3,19 @@ package io.so1s.backend.domain.deployment.controller;
 import io.so1s.backend.domain.deployment.dto.request.DeploymentRequestDto;
 import io.so1s.backend.domain.deployment.dto.response.DeploymentResponseDto;
 import io.so1s.backend.domain.deployment.entity.Deployment;
+import io.so1s.backend.domain.deployment.entity.DeploymentStrategy;
 import io.so1s.backend.domain.deployment.entity.Resource;
 import io.so1s.backend.domain.deployment.service.DeploymentService;
 import io.so1s.backend.domain.kubernetes.service.KubernetesService;
+import io.so1s.backend.domain.model.entity.ModelMetadata;
+import io.so1s.backend.domain.model.service.ModelService;
+import io.so1s.backend.global.error.exception.DeploymentNotFoundException;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,13 +26,38 @@ public class DeploymentController {
 
   private final DeploymentService deploymentService;
   private final KubernetesService kubernetesService;
+  private final ModelService modelService;
 
   @PostMapping
-  public ResponseEntity<DeploymentResponseDto> deploy(
+  public ResponseEntity<DeploymentResponseDto> createDeployment(
       @Valid @RequestBody DeploymentRequestDto deploymentRequestDto) {
 
     Resource resource = deploymentService.createResource(deploymentRequestDto.getResources());
     Deployment deployment = deploymentService.createDeployment(resource, deploymentRequestDto);
+
+    return ResponseEntity.ok(
+        DeploymentResponseDto.builder()
+            .success(kubernetesService.deployInferenceServer(deployment))
+            .id(deployment.getId())
+            .name(deployment.getName())
+            .build());
+  }
+
+  @PutMapping
+  public ResponseEntity<DeploymentResponseDto> updateDeployment(
+      @Valid @RequestBody DeploymentRequestDto deploymentRequestDto)
+      throws DeploymentNotFoundException {
+
+    Deployment deployment = deploymentService.validateExistDeployment(
+        deploymentRequestDto.getName());
+    
+    DeploymentStrategy deploymentStrategy = deploymentService.validateExistDeploymentStrategy(
+        deploymentRequestDto.getStrategy());
+    ModelMetadata modelMetadata = modelService.validateExistModelMetadata(
+        deploymentRequestDto.getModelMetadataId());
+    Resource resource = deploymentService.createResource(deploymentRequestDto.getResources());
+
+    deployment.update(modelMetadata, deploymentStrategy, resource);
 
     return ResponseEntity.ok(
         DeploymentResponseDto.builder()
