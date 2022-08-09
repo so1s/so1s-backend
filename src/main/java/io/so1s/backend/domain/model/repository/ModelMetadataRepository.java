3acab934660717b07524dc9aee7f42348f6643a9
shinilseop <shinilseop12@gmail.com>
2022-08-09 19:09:36 +0900

    feat: add find model metadata by model id

--- src/main/java/io/so1s/backend/domain/model/repository/ModelMetadataRepository.java
+++ src/main/java/io/so1s/backend/domain/model/repository/ModelMetadataRepository.java
@@ -4,6 +4,7 @@ import io.so1s.backend.domain.model.entity.ModelMetadata;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ModelMetadataRepository extends JpaRepository<ModelMetadata, Long> {
 
@@ -12,4 +13,7 @@ public interface ModelMetadataRepository extends JpaRepository<ModelMetadata, Lo
   Optional<ModelMetadata> findById(Long id);
 
   List<ModelMetadata> findAll();
+
+  @Query("select m from Model m where m.id = :id")
+  List<ModelMetadata> findByModelId(Long id);
 }
