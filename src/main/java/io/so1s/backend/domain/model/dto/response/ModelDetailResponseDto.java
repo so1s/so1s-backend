fdaab37ab880572b65ed8de6a913af50df8beaa7
shinilseop <shinilseop12@gmail.com>
2022-08-11 02:13:49 +0900

    chore: add columns to detail res

--- src/main/java/io/so1s/backend/domain/model/dto/response/ModelDetailResponseDto.java
+++ src/main/java/io/so1s/backend/domain/model/dto/response/ModelDetailResponseDto.java
@@ -1,6 +1,7 @@
 package io.so1s.backend.domain.model.dto.response;
 
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,9 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ModelDetailResponseDto {
 
+  private LocalDateTime age;
   private String name;
+  private String version;
   private String status;
   private String url;
   private String library;
