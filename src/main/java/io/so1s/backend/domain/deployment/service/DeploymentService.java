a0b921e1bbe94f386353f56623a3acc249f0e728
shinilseop <shinilseop12@gmail.com>
2022-08-17 01:56:43 +0900

    feat: add find deployment service method

--- src/main/java/io/so1s/backend/domain/deployment/service/DeploymentService.java
+++ src/main/java/io/so1s/backend/domain/deployment/service/DeploymentService.java
@@ -2,9 +2,11 @@ package io.so1s.backend.domain.deployment.service;
 
 import io.so1s.backend.domain.deployment.dto.request.DeploymentRequestDto;
 import io.so1s.backend.domain.deployment.dto.request.ResourceRequestDto;
+import io.so1s.backend.domain.deployment.dto.response.DeploymentFindResponseDto;
 import io.so1s.backend.domain.deployment.entity.Deployment;
 import io.so1s.backend.domain.deployment.entity.DeploymentStrategy;
 import io.so1s.backend.domain.deployment.entity.Resource;
+import java.util.List;
 
 public interface DeploymentService {
 
@@ -17,4 +19,10 @@ public interface DeploymentService {
   Deployment updateDeployment(DeploymentRequestDto deploymentRequestDto);
 
   Deployment validateExistDeployment(String name);
+
+  DeploymentFindResponseDto setDeploymentFindResponseDto(Deployment deployment);
+
+  List<DeploymentFindResponseDto> findDeployments();
+
+  DeploymentFindResponseDto findDeployment(Long id);
 }
