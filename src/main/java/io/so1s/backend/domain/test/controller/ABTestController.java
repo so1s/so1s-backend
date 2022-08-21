1c839fac4c81c251338e27aefe5a7297b9169a4d
Jiho Lee <optional.int@kakao.com>
2022-08-21 23:55:18 +0900

    feat: add ABTestController

---
+++ src/main/java/io/so1s/backend/domain/test/controller/ABTestController.java
@@ -0,0 +1,64 @@
+package io.so1s.backend.domain.test.controller;
+
+import io.so1s.backend.domain.deployment.service.DeploymentService;
+import io.so1s.backend.domain.test.dto.mapper.ABTestMapper;
+import io.so1s.backend.domain.test.dto.request.ABTestRequestDto;
+import io.so1s.backend.domain.test.dto.response.ABTestCreateResponseDto;
+import io.so1s.backend.domain.test.dto.response.ABTestReadResponseDto;
+import io.so1s.backend.domain.test.entity.ABTest;
+import io.so1s.backend.domain.test.service.ABTestService;
+import io.so1s.backend.global.error.exception.ABTestNotFoundException;
+import io.so1s.backend.global.error.exception.DeploymentNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/v1/tests/ab")
+@RequiredArgsConstructor
+public class ABTestController {
+
+  private final ABTestService abTestService;
+  private final DeploymentService deploymentService;
+  private final ABTestMapper mapper;
+
+  @PostMapping
+  public ResponseEntity<ABTestCreateResponseDto> createABTest(
+      @Valid @RequestBody ABTestRequestDto requestDto)
+      throws DeploymentNotFoundException, DataIntegrityViolationException {
+
+    ABTest abTest = abTestService.createABTest(requestDto);
+
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(mapper.toCreateDto("AB Test 객체가 생성되었습니다.", abTest));
+  }
+
+  @PutMapping
+  public ResponseEntity<ABTestCreateResponseDto> updateABTest(
+      @Valid @RequestBody ABTestRequestDto abTestRequestDto)
+      throws ABTestNotFoundException, DeploymentNotFoundException {
+
+    ABTest abTest = abTestService.updateABTest(abTestRequestDto);
+
+    return ResponseEntity.ok(mapper.toCreateDto("AB Test 객체가 변경되었습니다.", abTest));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<ABTestReadResponseDto>> findDeployments() {
+    return ResponseEntity.ok(abTestService.findAll());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ABTestReadResponseDto> findDeployment(
+      @Valid @PathVariable("id") Long id
+  ) throws ABTestNotFoundException {
+    return ResponseEntity.ok(abTestService.findbyId(id)
+        .orElseThrow(() -> new ABTestNotFoundException("주어진 AB Test id와 일치하는 객체를 찾지 못했습니다.")));
+  }
+}
