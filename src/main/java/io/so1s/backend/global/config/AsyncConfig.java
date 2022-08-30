69450c452ce71a6c2f8e4324b632c35e51b51c60
shinilseop <shinilseop12@gmail.com>
2022-08-30 18:35:49 +0900

    refactor: annotation sequence change

--- src/main/java/io/so1s/backend/global/config/AsyncConfig.java
+++ src/main/java/io/so1s/backend/global/config/AsyncConfig.java
@@ -1,22 +1,23 @@
 package io.so1s.backend.global.config;
 
 import java.util.concurrent.Executor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@Configuration
 @EnableAsync
-public class AsyncConfig {
+@Configuration
+public class AsyncConfig extends AsyncConfigurerSupport {
 
-  @Bean
-  public Executor threadPoolTaskExecutor() {
-    ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
-    taskExecutor.setCorePoolSize(1);
-    taskExecutor.setMaxPoolSize(30);
-    taskExecutor.setQueueCapacity(100);
-    taskExecutor.setThreadNamePrefix("Async-Executor-");
-    return taskExecutor;
+  @Override
+  public Executor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(3);
+    executor.setMaxPoolSize(30);
+    executor.setQueueCapacity(50);
+    executor.setThreadNamePrefix("Async-Executor-");
+    executor.initialize();
+    return executor;
   }
 }
