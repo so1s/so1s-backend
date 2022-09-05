0029e5b5fa6e91e7277b2edd4a0c4039f10991b3
Jiho Lee <optional.int@kakao.com>
2022-09-05 23:36:41 +0900

    feat: add deleteModel / ModelMetadata

--- src/main/java/io/so1s/backend/domain/model/service/ModelService.java
+++ src/main/java/io/so1s/backend/domain/model/service/ModelService.java
@@ -2,13 +2,17 @@ package io.so1s.backend.domain.model.service;
 
 import io.so1s.backend.domain.aws.dto.response.FileSaveResultForm;
 import io.so1s.backend.domain.model.dto.request.ModelUploadRequestDto;
+import io.so1s.backend.domain.model.dto.response.ModelDeleteResponseDto;
 import io.so1s.backend.domain.model.dto.response.ModelDetailResponseDto;
 import io.so1s.backend.domain.model.dto.response.ModelFindResponseDto;
+import io.so1s.backend.domain.model.dto.response.ModelMetadataDeleteResponseDto;
 import io.so1s.backend.domain.model.dto.response.ModelMetadataFindResponseDto;
 import io.so1s.backend.domain.model.entity.Library;
 import io.so1s.backend.domain.model.entity.Model;
 import io.so1s.backend.domain.model.entity.ModelMetadata;
+import io.so1s.backend.global.error.exception.DeploymentExistsException;
 import io.so1s.backend.global.error.exception.LibraryNotFoundException;
+import io.so1s.backend.global.error.exception.ModelMetadataNotFoundException;
 import io.so1s.backend.global.error.exception.ModelNotFoundException;
 import java.util.List;
 
@@ -32,4 +36,9 @@ public interface ModelService {
   List<ModelMetadataFindResponseDto> findModelMetadatasByModelId(Long id);
 
   ModelDetailResponseDto findModelDetail(Long modelId, String version);
+
+  ModelDeleteResponseDto deleteModel(Long modelId) throws ModelNotFoundException;
+
+  ModelMetadataDeleteResponseDto deleteModelMetadata(Long modelId, String version)
+      throws ModelMetadataNotFoundException, DeploymentExistsException;
 }
