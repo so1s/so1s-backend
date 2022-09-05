dbe53f42ebee44915b7272321c1c922761812740
Jiho Lee <optional.int@kakao.com>
2022-09-05 15:17:01 +0900

    feat: add deleteDeployment service method

--- src/main/java/io/so1s/backend/domain/deployment/service/DeploymentService.java
+++ src/main/java/io/so1s/backend/domain/deployment/service/DeploymentService.java
@@ -2,11 +2,12 @@ package io.so1s.backend.domain.deployment.service;
 
 import io.so1s.backend.domain.deployment.dto.request.DeploymentRequestDto;
 import io.so1s.backend.domain.deployment.dto.request.ResourceRequestDto;
+import io.so1s.backend.domain.deployment.dto.response.DeploymentDeleteResponseDto;
 import io.so1s.backend.domain.deployment.dto.response.DeploymentFindResponseDto;
 import io.so1s.backend.domain.deployment.entity.Deployment;
 import io.so1s.backend.domain.deployment.entity.DeploymentStrategy;
 import io.so1s.backend.domain.deployment.entity.Resource;
-
+import io.so1s.backend.global.error.exception.DeploymentNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +16,8 @@ public interface DeploymentService {
   Resource createResource(ResourceRequestDto resourceRequestDto);
 
   Deployment createDeployment(Resource resource, DeploymentRequestDto deploymentRequestDto);
+
+  DeploymentDeleteResponseDto deleteDeployment(Long id) throws DeploymentNotFoundException;
 
   DeploymentStrategy validateExistDeploymentStrategy(String name);
 
