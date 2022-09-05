0c4ad52c63054a34b41ea42b5f3797416d59497f
Jiho Lee <optional.int@kakao.com>
2022-09-05 23:35:25 +0900

    feat: add deleteFile method using S3

--- src/main/java/io/so1s/backend/domain/aws/service/UploadService.java
+++ src/main/java/io/so1s/backend/domain/aws/service/UploadService.java
@@ -8,4 +8,6 @@ public interface UploadService {
   void uploadFile(InputStream inputStream, ObjectMetadata objectMetadata, String fileName);
 
   String getFileUrl(String fileName);
+
+  void deleteFile(String url);
 }
