9009f74f6ea734dc08bfb3af0571399db3cc73b1
shinilseop <shinilseop12@gmail.com>
2022-09-07 04:17:37 +0900

    chore: change valid type

--- src/main/java/io/so1s/backend/domain/deployment/dto/request/DeploymentRequestDto.java
+++ src/main/java/io/so1s/backend/domain/deployment/dto/request/DeploymentRequestDto.java
@@ -1,6 +1,8 @@
 package io.so1s.backend.domain.deployment.dto.request;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,16 +17,16 @@ public class DeploymentRequestDto {
 
 
   @NotBlank
-  @Size(min = 3, max = 100)
+  @Size(min = 1, max = 64, message = "Please enter the length within 1 to 64 characters.")
   private String name;
 
-  @NotBlank
+  @NotNull(message = "Please Input ModelMetadataId.")
   private Long modelMetadataId;
 
   @Builder.Default
   private String strategy = "rolling";
 
-  @NotBlank
+  @Valid
   private ResourceRequestDto resources;
 
 }
