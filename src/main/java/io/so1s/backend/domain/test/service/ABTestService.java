87a522126287845c4d79fa8d4280282f8425d712
Jiho Lee <optional.int@kakao.com>
2022-09-06 19:42:08 +0900

    chore: change dto names

--- src/main/java/io/so1s/backend/domain/test/service/ABTestService.java
+++ src/main/java/io/so1s/backend/domain/test/service/ABTestService.java
@@ -5,15 +5,14 @@ import io.so1s.backend.domain.test.dto.response.ABTestReadResponseDto;
 import io.so1s.backend.domain.test.entity.ABTest;
 import io.so1s.backend.global.error.exception.ABTestNotFoundException;
 import io.so1s.backend.global.error.exception.DeploymentNotFoundException;
-
 import java.util.List;
 import java.util.Optional;
 
 public interface ABTestService {
 
-  ABTest createABTest(ABTestRequestDto deploymentRequestDto) throws DeploymentNotFoundException;
+  ABTest createABTest(ABTestRequestDto abTestRequestDto) throws DeploymentNotFoundException;
 
-  ABTest updateABTest(ABTestRequestDto deploymentRequestDto)
+  ABTest updateABTest(ABTestRequestDto abTestRequestDto)
       throws ABTestNotFoundException, DeploymentNotFoundException;
 
   List<ABTestReadResponseDto> findAll();
