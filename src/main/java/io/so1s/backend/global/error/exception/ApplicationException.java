65eec096de48f0e081c6049dcefd00631d6482e1
shinilseop <shinilseop12@gmail.com>
2022-09-08 00:03:01 +0900

    chore: remove field error

--- src/main/java/io/so1s/backend/global/error/exception/ApplicationException.java
+++ src/main/java/io/so1s/backend/global/error/exception/ApplicationException.java
@@ -1,23 +1,14 @@
 package io.so1s.backend.global.error.exception;
 
-import io.so1s.backend.global.error.ErrorResponseDto.FieldError;
 import lombok.Getter;
 
 @Getter
 public class ApplicationException extends RuntimeException {
 
   private ErrorCode errorCode;
-  private FieldError fieldError;
 
   public ApplicationException(String message, ErrorCode errorCode) {
     super(message);
     this.errorCode = errorCode;
-    fieldError = null;
-  }
-
-  public ApplicationException(String message, ErrorCode errorCode, FieldError fieldError) {
-    super(message);
-    this.errorCode = errorCode;
-    this.fieldError = fieldError;
   }
 }
