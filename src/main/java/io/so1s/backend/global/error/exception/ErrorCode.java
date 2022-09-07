3209348a7c9029c47bb7c4d9ab0897efd034da26
shinilseop <shinilseop12@gmail.com>
2022-09-07 23:04:45 +0900

    chore: change errorcode sequence

--- src/main/java/io/so1s/backend/global/error/exception/ErrorCode.java
+++ src/main/java/io/so1s/backend/global/error/exception/ErrorCode.java
@@ -13,10 +13,10 @@ public enum ErrorCode {
   //Common
   INVALID_INPUT_VALUE(400, "Common001", "Invalid Input Value"),
   METHOD_NOT_ALLOWED(405, "Common002", "Invalid Input Value"),
-  ENTITY_NOT_FOUND(404, "Common003", "Entity Not Found"),
-  INTERNAL_SERVER_ERROR(500, "Common004", "Server Error"),
-  INVALID_TYPE_VALUE(400, "Common005", "Invalid Type Value"),
-  HANDLE_ACCESS_DENIED(403, "Common006", "Access is Denied"),
+  INVALID_TYPE_VALUE(400, "Common003", "Invalid Type Value"),
+  HANDLE_ACCESS_DENIED(403, "Common004", "Access is Denied"),
+  INTERNAL_SERVER_ERROR(500, "Common005", "Server Error"),
+  ENTITY_NOT_FOUND(404, "Common006", "Entity Not Found"),
   ENTITY_DUPLICATED(409, "Common007", "Entity Duplicated"),
   ENTITY_EXIST(409, "Common008", "Entity Exist"),
 
