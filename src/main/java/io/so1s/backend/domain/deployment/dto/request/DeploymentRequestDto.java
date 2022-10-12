6f610c97220db31e8df42505ea3ce85f4c6198d5
Jiho Lee <optional.int@kakao.com>
2022-10-12 20:39:36 +0900

    fix: test failure

--- src/main/java/io/so1s/backend/domain/deployment/dto/request/DeploymentRequestDto.java
+++ src/main/java/io/so1s/backend/domain/deployment/dto/request/DeploymentRequestDto.java
@@ -1,5 +1,6 @@
 package io.so1s.backend.domain.deployment.dto.request;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
