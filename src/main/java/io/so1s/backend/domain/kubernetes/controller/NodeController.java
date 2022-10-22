0a853f21f5a27603a2074782d2297cc1a61f381d
Jiho Lee <optional.int@kakao.com>
2022-10-22 23:41:42 +0900

    feat: implement node yaml GET API

--- src/main/java/io/so1s/backend/domain/kubernetes/controller/NodeController.java
+++ src/main/java/io/so1s/backend/domain/kubernetes/controller/NodeController.java
@@ -1,11 +1,14 @@
 package io.so1s.backend.domain.kubernetes.controller;
 
 import io.fabric8.kubernetes.api.model.Node;
+import io.so1s.backend.domain.kubernetes.service.KubernetesService;
 import io.so1s.backend.domain.kubernetes.service.NodesService;
 import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,10 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class NodeController {
 
   private final NodesService nodesService;
+  private final KubernetesService kubernetesService;
 
   @GetMapping
   public ResponseEntity<List<Node>> getNodes() {
     return ResponseEntity.ok(nodesService.findNodes());
+  }
+
+  @GetMapping("/{node_name}/yaml")
+  public ResponseEntity<String> findDeploymentYaml(
+      @Valid @PathVariable("node_name") String nodeName) {
+    Node node = nodesService.findNodeByName(nodeName);
+    String yaml = kubernetesService.getWorkloadToYaml(node);
+
+    return ResponseEntity.ok(yaml);
   }
 
 }
--- src/main/java/io/so1s/backend/domain/kubernetes/service/NodesServiceImpl.java
+++ src/main/java/io/so1s/backend/domain/kubernetes/service/NodesServiceImpl.java
@@ -2,7 +2,9 @@ package io.so1s.backend.domain.kubernetes.service;
 
 import io.fabric8.kubernetes.api.model.Node;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import io.so1s.backend.domain.kubernetes.exception.NodeNotFoundException;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,13 @@ public class NodesServiceImpl implements NodesService {
   @Override
   public List<Node> findNodes() {
     return client.nodes().list().getItems();
+  }
+
+  @Override
+  public Node findNodeByName(String name) throws NodeNotFoundException {
+    return Optional.ofNullable(client.nodes().withName(name).get())
+        .orElseThrow(() -> new NodeNotFoundException(
+            String.format("Node %s not found.", name)));
   }
 
 }
