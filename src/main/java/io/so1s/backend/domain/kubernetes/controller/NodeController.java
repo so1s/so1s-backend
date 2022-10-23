ab94f298306f18ee14ad6f1e8a143d159d97afb7
Jiho Lee <optional.int@kakao.com>
2022-10-23 17:02:55 +0900

    refactor: use node yaml response dto

--- src/main/java/io/so1s/backend/domain/kubernetes/controller/NodeController.java
+++ src/main/java/io/so1s/backend/domain/kubernetes/controller/NodeController.java
@@ -1,6 +1,7 @@
 package io.so1s.backend.domain.kubernetes.controller;
 
 import io.fabric8.kubernetes.api.model.Node;
+import io.so1s.backend.domain.kubernetes.dto.response.NodeFindResponseDto;
 import io.so1s.backend.domain.kubernetes.service.KubernetesService;
 import io.so1s.backend.domain.kubernetes.service.NodesService;
 import java.util.List;
@@ -26,12 +27,12 @@ public class NodeController {
   }
 
   @GetMapping("/{node_name}/yaml")
-  public ResponseEntity<String> findDeploymentYaml(
+  public ResponseEntity<NodeFindResponseDto> findDeploymentYaml(
       @Valid @PathVariable("node_name") String nodeName) {
     Node node = nodesService.findNodeByName(nodeName);
     String yaml = kubernetesService.getWorkloadToYaml(node);
 
-    return ResponseEntity.ok(yaml);
+    return ResponseEntity.ok(NodeFindResponseDto.builder().yaml(yaml).build());
   }
 
 }
