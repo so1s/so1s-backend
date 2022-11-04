f4b963f2c2372fd4ea2470515db26c4615310841
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-11-04 23:05:50 +0900

    refactor: remove code

--- src/main/java/io/so1s/backend/domain/auth/bean/AfterStartupHandler.java
+++ src/main/java/io/so1s/backend/domain/auth/bean/AfterStartupHandler.java
@@ -3,7 +3,6 @@ package io.so1s.backend.domain.auth.bean;
 import io.so1s.backend.domain.auth.exception.DuplicatedUserException;
 import io.so1s.backend.domain.auth.service.UserService;
 import io.so1s.backend.domain.auth.vo.UserRole;
-import io.so1s.backend.domain.kubernetes.service.KubernetesService;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -13,13 +12,13 @@ import org.springframework.stereotype.Component;
 public class AfterStartupHandler {
 
   private final UserService userService;
-  private final KubernetesService kubernetesService;
+//  private final KubernetesService kubernetesService;
 
   @PostConstruct
   public void init() {
     try {
       userService.createUser("so1s", "admin12345", UserRole.ADMIN);
-      kubernetesService.createNamespace("so1s");
+//      kubernetesService.createNamespace("so1s");
     } catch (DuplicatedUserException ignored) {
     }
   }
