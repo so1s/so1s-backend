3e2813bfebc83f17420bfece5f2ef25db9e54290
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-11-04 23:55:37 +0900

    fix: add check connection mehod

--- src/main/java/io/so1s/backend/domain/auth/bean/AfterStartupHandler.java
+++ src/main/java/io/so1s/backend/domain/auth/bean/AfterStartupHandler.java
@@ -1,8 +1,12 @@
 package io.so1s.backend.domain.auth.bean;
 
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClientException;
 import io.so1s.backend.domain.auth.exception.DuplicatedUserException;
 import io.so1s.backend.domain.auth.service.UserService;
 import io.so1s.backend.domain.auth.vo.UserRole;
+import io.so1s.backend.domain.kubernetes.service.KubernetesService;
+import java.net.UnknownHostException;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -12,14 +16,28 @@ import org.springframework.stereotype.Component;
 public class AfterStartupHandler {
 
   private final UserService userService;
-//  private final KubernetesService kubernetesService;
+  private final KubernetesService kubernetesService;
+  private final KubernetesClient client;
 
   @PostConstruct
   public void init() {
     try {
       userService.createUser("so1s", "admin12345", UserRole.ADMIN);
-//      kubernetesService.createNamespace("so1s");
+      if (isConnected()) {
+        kubernetesService.createNamespace("so1s");
+      }
     } catch (DuplicatedUserException ignored) {
     }
+  }
+
+  private boolean isConnected() {
+    try {
+      client.getVersion();
+    } catch (KubernetesClientException e) {
+      if (e.getCause() instanceof UnknownHostException) {
+        return false;
+      }
+    }
+    return true;
   }
 }
