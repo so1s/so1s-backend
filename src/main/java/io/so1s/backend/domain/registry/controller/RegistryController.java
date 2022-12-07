d082d3b5814b7fac1915c506153a7b0dd79cc229
Jiho Lee <optional.int@kakao.com>
2022-12-07 12:54:58 +0900

    chore: change endpoint

--- src/main/java/io/so1s/backend/domain/registry/controller/RegistryController.java
+++ src/main/java/io/so1s/backend/domain/registry/controller/RegistryController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v2/registry")
+@RequestMapping("/api/v2/registries")
 @RequiredArgsConstructor
 public class RegistryController {
 
