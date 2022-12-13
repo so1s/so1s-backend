b3222a996313b42c0457b72e903445a06f6669b0
Jiho Lee <optional.int@kakao.com>
2022-12-13 15:11:56 +0900

    fix: KubernetesInitializer working on test

--- src/main/java/io/so1s/backend/global/initializer/common/KubernetesInitializer.java
+++ src/main/java/io/so1s/backend/global/initializer/common/KubernetesInitializer.java
@@ -3,8 +3,10 @@ package io.so1s.backend.global.initializer.common;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Component
 @RequiredArgsConstructor
 public class KubernetesInitializer {
